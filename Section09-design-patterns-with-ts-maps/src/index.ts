010ff406594f4ad5a9081df5fba0de0c449ff5d9
Irene <irenesis123@gmail.com>
2021-01-05 10:46:21 -0500

    60. Expoloring Type Definition Files + rm parcel .cache

--- Section09-design-patterns-with-ts-maps/src/index.ts
+++ Section09-design-patterns-with-ts-maps/src/index.ts
@@ -8,4 +8,10 @@ console.log(user);
 const company = new Company();
 console.log(company);
 
-new google.maps.Map(document.getElementById('map'));
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
