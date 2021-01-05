3c9ce92485001c3de4291eeaaeaa31a347fd641d
Irene <irenesis123@gmail.com>
2021-01-05 09:30:58 -0500

    56. Defining a Company

---
+++ Section09-design-patterns-with-ts-maps/src/User.ts
@@ -0,0 +1,21 @@
+import faker from 'faker';
+
+export class User {
+    name: string;
+    // type an obj 
+    location: {
+        lat: number;
+        lng: number;
+    }
+
+    // initialize the info 
+    constructor() {
+        this.name = faker.name.firstName();
+        // initialize objects in constructors as such. this.location.lat = 12 won't work
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
+}
+
