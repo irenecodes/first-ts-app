91cfd65e820494084eb6faa768873e771bb6d0f2
Irene <irenesis123@gmail.com>
2021-01-07 00:10:51 -0500

    74. Concurrent compilation and execution

---
+++ Section10-more-on-design-patterns-sort/build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('hi');
+function logWhatever(string) {
+    console.log(string);
+}
+;
+logWhatever('bun');
+var logSomething = function () {
+    console.log('something yooo');
+};
+logSomething();
---
+++ Section10-more-on-design-patterns-sort/src/index.ts
@@ -0,0 +1,14 @@
+console.log('hi');
+
+function logWhatever(string: string) {
+    console.log(string);
+  };
+  
+logWhatever('bun');
+
+const logSomething = () => {
+    console.log('something yooo');
+    
+}
+
+logSomething()
