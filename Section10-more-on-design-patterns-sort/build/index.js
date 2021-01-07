4d5ed2e64a87e2a0349e5304d4791477f71326ab
Irene <irenesis123@gmail.com>
2021-01-07 13:18:54 -0500

    80. Type Guards

--- Section10-more-on-design-patterns-sort/build/index.js
+++ Section10-more-on-design-patterns-sort/build/index.js
@@ -1,11 +1,30 @@
 "use strict";
-console.log('hi');
-function logWhatever(string) {
-    console.log(string);
-}
-;
-logWhatever('bun');
-var logSomething = function () {
-    console.log('something yooo');
-};
-logSomething();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        // this.collection.indexOf
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - 1 - 1; j++) {
+                //type guard to restore all methods associated w array
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // type guard if string to restore methods for strings 
+                if (typeof this.collection === 'string') {
+                    console.log('type guard for string');
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- Section10-more-on-design-patterns-sort/src/index.ts
+++ Section10-more-on-design-patterns-sort/src/index.ts
@@ -1,14 +1,31 @@
-console.log('hi');
+class Sorter {
+    constructor(public collection: number[] | string){}
+    sort(): void {
+        const {length} = this.collection;
+        // this.collection.indexOf
 
-function logWhatever(string: string) {
-    console.log(string);
-  };
-  
-logWhatever('bun');
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - 1 - 1; j++) {
 
-const logSomething = () => {
-    console.log('something yooo');
-    
+                //type guard to restore all methods associated w array
+                if (this.collection instanceof Array){
+                    if(this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j]
+                        this.collection[j] = this.collection[j+1];
+                        this.collection[j+1] = leftHand;
+                    }
+                }
+
+                // type guard if string to restore methods for strings 
+                if (typeof this.collection === 'string') {
+                    console.log('type guard for string');
+                    
+                }
+            }
+        }
+    }
 }
 
-logSomething()
+const sorter = new Sorter([10, 3, -5, 0])
+sorter.sort();
+console.log(sorter.collection);
