ee8b4a424f1fcd9364b1afd4bd2a9e33735f8c5c
Irene <irenesis123@gmail.com>
2021-01-16 23:37:16 -0500

    103. Using Enums

--- Section11-reusable-code-stats/build/index.js
+++ Section11-reusable-code-stats/build/index.js
@@ -9,13 +9,36 @@ var matches = fs_1.default.readFileSync('football.csv', {
 }).split('\n').map(function (row) {
     return row.split(',');
 });
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
+// JS solution putting it in an obj so even if any of these won't be used, another dev won't be tempted to delete 
+// const MatchResult = {
+//     HomeWin: 'H',
+//     AwayWin: 'A',
+//     Draw: 'D'
+// }
+// TS solution - using enumerations 
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+// when create an enum, we create a TYPE of MatchResult as well 
+// example on lines 29-33 but it will not run. it is just an example to show that the annotation should return type of MatchResult
+var printMatchResult = function () {
+    if (match[5] === 'H') {
+        return MatchResult.HomeWin;
+    }
+};
 var manUnitedWins = 0;
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- Section11-reusable-code-stats/src/index.ts
+++ Section11-reusable-code-stats/src/index.ts
@@ -6,13 +6,38 @@ const matches = fs.readFileSync('football.csv', {
     return row.split(',');
 })
 
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
+
+// JS solution putting it in an obj so even if any of these won't be used, another dev won't be tempted to delete 
+// const MatchResult = {
+//     HomeWin: 'H',
+//     AwayWin: 'A',
+//     Draw: 'D'
+// }
+
+// TS solution - using enumerations 
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+}
+
+// when create an enum, we create a TYPE of MatchResult as well 
+// example on lines 29-33 but it will not run. it is just an example to show that the annotation should return type of MatchResult
+const printMatchResult = (): MatchResult => {
+    if(match[5] === 'H') {
+        return MatchResult.HomeWin
+    }
+}
 
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
