335dcb410dff0db1c84d50dc97e3462c60ce332e
Irene <irenesis123@gmail.com>
2021-01-17 22:45:26 -0500

    105. Extracting CSV Reading

--- Section11-reusable-code-stats/build/index.js
+++ Section11-reusable-code-stats/build/index.js
@@ -1,23 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n').map(function (row) {
-    return row.split(',');
-});
-// const homeWin = 'H';
-// const awayWin = 'A';
-// const draw = 'D';
-// JS solution putting it in an obj so even if any of these won't be used, another dev won't be tempted to delete 
-// const MatchResult = {
-//     HomeWin: 'H',
-//     AwayWin: 'A',
-//     Draw: 'D'
-// }
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
 // TS solution - using enumerations 
 var MatchResult;
 (function (MatchResult) {
@@ -25,16 +10,9 @@ var MatchResult;
     MatchResult["AwayWin"] = "A";
     MatchResult["Draw"] = "D";
 })(MatchResult || (MatchResult = {}));
-// when create an enum, we create a TYPE of MatchResult as well 
-// example on lines 29-33 but it will not run. it is just an example to show that the annotation should return type of MatchResult
-var printMatchResult = function () {
-    if (match[5] === 'H') {
-        return MatchResult.HomeWin;
-    }
-};
 var manUnitedWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
--- Section11-reusable-code-stats/src/index.ts
+++ Section11-reusable-code-stats/src/index.ts
@@ -1,21 +1,8 @@
-import fs from 'fs';
+import {CsvFileReader} from './CsvFileReader';
 
-const matches = fs.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n').map((row: string): string[] => {
-    return row.split(',');
-})
+const reader = new CsvFileReader('football.csv');
+reader.read(); 
 
-// const homeWin = 'H';
-// const awayWin = 'A';
-// const draw = 'D';
-
-// JS solution putting it in an obj so even if any of these won't be used, another dev won't be tempted to delete 
-// const MatchResult = {
-//     HomeWin: 'H',
-//     AwayWin: 'A',
-//     Draw: 'D'
-// }
 
 // TS solution - using enumerations 
 enum MatchResult {
@@ -24,17 +11,10 @@ enum MatchResult {
     Draw = 'D'
 }
 
-// when create an enum, we create a TYPE of MatchResult as well 
-// example on lines 29-33 but it will not run. it is just an example to show that the annotation should return type of MatchResult
-const printMatchResult = (): MatchResult => {
-    if(match[5] === 'H') {
-        return MatchResult.HomeWin
-    }
-}
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
