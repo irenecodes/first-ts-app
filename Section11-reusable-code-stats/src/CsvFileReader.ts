ec0efbf71ab1c79edf1f58f8e672d0e738a91302
Irene <irenesis123@gmail.com>
2021-01-19 00:54:44 -0500

    115. Applying a Type to a generic class

--- Section11-reusable-code-stats/src/CsvFileReader.ts
+++ Section11-reusable-code-stats/src/CsvFileReader.ts
@@ -1,14 +1,14 @@
 import fs from 'fs';
-import {dateStringToDate} from './utils'
-import {MatchResult} from './MatchResult'
-
-// creating a tuple structure
-type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 
-export class CsvFileReader {
-    data: MatchData[] = [];
+
+
+export abstract class CsvFileReader<T> {
+    data: T[] = [];
     constructor(public filename: string) {}
+
+    // mark this method as abstract which means it will be implemented by child class
+    abstract mapRow(row:string[]): T;
 
     read() {
         this.data = fs.readFileSync(this.filename, {
@@ -21,17 +21,5 @@ export class CsvFileReader {
         }) 
         // conversion process for each of the values 
         .map(this.mapRow)
-    }
-
-    mapRow(row:string[]): MatchData {
-        return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult, 
-            row[6]
-        ]
     }
 }
--- Section11-reusable-code-stats/src/index.ts
+++ Section11-reusable-code-stats/src/index.ts
@@ -1,8 +1,8 @@
 import { table } from 'console';
-import {CsvFileReader} from './CsvFileReader';
+import {MatchReader} from './MatchReader';
 import {MatchResult} from './MatchResult'
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read(); 
 
 
